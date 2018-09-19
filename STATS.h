98389be1cd76723d5892f4f0883c5ded1a2e984f
Straikiss <starko.web@gmail.com>
2018-09-20 01:48:20 +0300

    STATS

---
+++ STATS.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef _STATS_H_
+#define _STATS_H_
+
+struct Stats
+{
+	string Nickname;
+	int Health;
+	int Might;
+	double Defence;
+	int CritMight;
+	int CritChance;
+	int CritBonus;
+	int Crit;
+	int ResetMight;
+	int Regen;
+	int Dodge;
+	int DodgeChange;
+} Player, Enemy;
+
+#endif
