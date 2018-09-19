ae9769291a3bcff2e61960a9dcaead1963875e6e
Straikiss <starko.web@gmail.com>
2018-09-20 01:47:12 +0300

    Includes

---
+++ Includes.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef _Includes_H_
+#define _Includes_H_
+
+#include <windows.h>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+#include "STATS.h"
+#include "DisplaySleep.h"
+#include "SetupPlayer.h"
+#include "SetupEnemy.h"
+#include "MAIN.h"
+
+#endif
