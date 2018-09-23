f3f87e9cf47c08f8a57029961e7a53a0f435c485
Straikiss <starko.web@gmail.com>
2018-09-23 21:51:54 +0300

    SetupEnemy

--- SetupEnemy.h
+++ SetupEnemy.h
@@ -3,69 +3,87 @@
 #ifndef _SetupEnemy_H_
 #define _SetupEnemy_H_
 
-void EnemySetup()
+using std::string;
+using std::istringstream;
+
+void SetupEnemy()
 {
 	// Getting Enemy.Stats from DBEnemy
-	string GetEnemyNickname;
-	string GetEnemyHealth;
-	string GetEnemyMight;
-	string GetEnemyDefence;
-	string GetEnemyCritChance;
-	string GetEnemyCritBonus;
-	string GetEnemyRegen;
+	string EnemyNickname;
+	string EnemyHealth;
+	string EnemyMight;
+	string EnemyDefence;
+	string EnemyCritChance;
+	string EnemyCritBonus;
+	string EnemyRegen;
+	string EnemyDodgeChange;
 
 	// Reading Enemy.Stats from DBEnemy
-	ofstream DBEnemy;
-	ifstream ReadFromDBEnemy("DBEnemy.txt");
-	getline(ReadFromDBEnemy, GetEnemyNickname);
-	getline(ReadFromDBEnemy, GetEnemyHealth);
-	getline(ReadFromDBEnemy, GetEnemyMight);
-	getline(ReadFromDBEnemy, GetEnemyDefence);
-	getline(ReadFromDBEnemy, GetEnemyCritChance);
-	getline(ReadFromDBEnemy, GetEnemyCritBonus);
-	getline(ReadFromDBEnemy, GetEnemyRegen);
+	std::ofstream DBEnemy;
+	std::ifstream ReadFromDBEnemy("DBEnemy.txt");
+	std::getline(ReadFromDBEnemy, EnemyNickname);
+	getline(ReadFromDBEnemy, EnemyHealth);
+	getline(ReadFromDBEnemy, EnemyMight);
+	getline(ReadFromDBEnemy, EnemyDefence);
+	getline(ReadFromDBEnemy, EnemyCritChance);
+	getline(ReadFromDBEnemy, EnemyCritBonus);
+	getline(ReadFromDBEnemy, EnemyRegen);
+	getline(ReadFromDBEnemy, EnemyDodgeChange);
 	DBEnemy.close();
 
 	// Read only after =
-	GetEnemyNickname.erase(0, 17);
-	GetEnemyHealth.erase(0, 15);
-	GetEnemyMight.erase(0, 14);
-	GetEnemyDefence.erase(0, 16);
-	GetEnemyCritChance.erase(0, 19);
-	GetEnemyCritBonus.erase(0, 18);
-	GetEnemyRegen.erase(0, 14);
+	EnemyNickname.erase(0, 17);
+	EnemyHealth.erase(0, 15);
+	EnemyMight.erase(0, 14);
+	EnemyDefence.erase(0, 16);
+	EnemyCritChance.erase(0, 19);
+	EnemyCritBonus.erase(0, 18);
+	EnemyRegen.erase(0, 14);
+	EnemyDodgeChange.erase(0, 20);
 
-	// Setting stats from DBPlayer to Player.Stats
-	Enemy.Nickname = GetEnemyNickname;
-	istringstream(GetEnemyHealth) >> Enemy.Health;
-	istringstream(GetEnemyMight) >> Enemy.Might;
-	istringstream(GetEnemyDefence) >> Enemy.Defence;
-	istringstream(GetEnemyCritChance) >> Enemy.CritChance;
-	istringstream(GetEnemyCritBonus) >> Enemy.CritBonus;
-	istringstream(GetEnemyRegen) >> Enemy.Regen;
+	// Setting stats from DBPlayer to Enemy.Stats
+	Enemy.Nickname = EnemyNickname;
+	istringstream(EnemyHealth) >> Enemy.Health;
+	istringstream(EnemyMight) >> Enemy.Might;
+	istringstream(EnemyDefence) >> Enemy.Defence;
+	istringstream(EnemyCritChance) >> Enemy.CritChance;
+	istringstream(EnemyCritBonus) >> Enemy.CritBonus;
+	istringstream(EnemyRegen) >> Enemy.Regen;
+	istringstream(EnemyDodgeChange) >> Enemy.DodgeChange;
 
-	// 1 is min Health
 	if(Enemy.Health == 0)
 	{
 		Enemy.Health = 100;
 	}
 
-	// 1 is min CritChance
+	if(Enemy.Might == 0)
+	{
+		Enemy.Might = 10;
+	}
+
+	if(Enemy.Defence == 0)
+	{
+		Enemy.Defence = 1;
+	}
+
+	if(Enemy.Defence > 70)
+	{
+		Enemy.Defence = 70;
+	}
+
 	if(Enemy.CritChance == 0)
 	{
 		Enemy.CritChance = 1;
 	}
 
-	// 1 is min CritBonus
 	if(Enemy.CritBonus == 0)
 	{
 		Enemy.CritBonus = 1;
 	}
 
-	// 70 - 70% is max
-	if(Enemy.Defence > 70)
+	if(Enemy.Regen == 0)
 	{
-		Enemy.Defence = 70;
+		Enemy.Regen = 5;
 	}
 }
 
