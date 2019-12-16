9fcc88397711c4931d559d4158a44b39f35075ca
Alesey Kosenkov <starko.web@gmail.com>
2019-12-17 01:40:10 +0300

    Set console color in windows shell

---
+++ SetConsoleColor.h
@@ -0,0 +1,22 @@
+#ifndef _SETCONSOLECOLOR_H_
+#define _SETCONSOLECOLOR_H_
+
+void SetConsoleColor(std::string Color)
+{
+	if(Color == "Red")
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+	}
+
+	if(Color == "Green")
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
+	}
+
+	if(Color == "Black")
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	}
+}
+
+#endif // _SETCONSOLEOLOR_H_
