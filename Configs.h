f897262b76be62a0d4dd17ee5e4d8ad7183cef6b
Alesey Kosenkov <starko.web@gmail.com>
2019-12-17 01:41:53 +0300

    Stats

---
+++ Configs.h
@@ -0,0 +1,22 @@
+#ifndef _CONFIGS_H_
+#define _CONFIGS_H_
+
+struct Stats
+{
+	std::string Nickname;
+	int Health;
+	int Might;
+	double Defence;
+	int CritChance;
+	int CritBonus;
+	int Regen;
+	int DodgeChange;
+	double Lifesteal;
+	int Crit;
+	int Dodge;
+	int CritMight;
+	int LifestealHealth;
+	int ResetMight;
+} Player, Enemy;
+
+#endif // _CONFIGS_H_
